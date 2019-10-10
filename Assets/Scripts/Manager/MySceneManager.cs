7834adaa8a104c864e3e8d08cac6ae38e889c828
nagaoyosuke <hw17a124@oecu.jp>
2019-10-10 14:48:42 +0900

    ファーストコミット

---
+++ Assets/Scripts/Manager/MySceneManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MySceneManager : SingletonMonoBehaviour<MySceneManager> {
+
+	public static void GoTitle(){
+		SceneManager.LoadScene("Title");
+	}
+
+    public static void GoMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public static void GoMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+	public static void GoTutorial(){
+		SceneManager.LoadScene("Tutorial");
+	}
+
+	public static void GoResult(){
+		SceneManager.LoadScene("Result");
+	}
+
+	public static void GoModeSelect(){
+		SceneManager.LoadScene("ModeSelect");
+	}
+
+	public static void GoCredit(){
+		SceneManager.LoadScene("Credit");
+	}
+
+	public static void GoExplanatoryText(){
+		SceneManager.LoadScene("ExplanatoryText");
+	}
+
+}
---
+++ Assets/Scripts/Manager/StageMakeManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageMakeManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Stage1;
+    [SerializeField]
+    private GameObject Stage2;
+    [SerializeField]
+    private GameObject Stage3;
+
+    [SerializeField]
+    private Vector3 StagePosition;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Make();
+    }
+
+	private IEnumerator Start()
+	{
+		yield return new WaitForFixedUpdate();
+		Save.windZ = 10;
+	}
+
+	void Make()
+    {
+        GameObject Stage = null;
+        switch (Save.stageState)
+        {
+            case Save.StageState.STAGE1:
+                Stage = Instantiate(Stage1);
+                break;
+            case Save.StageState.STAGE2:
+                Stage = Instantiate(Stage2);
+                break;
+            case Save.StageState.STAGE3:
+                Stage = Instantiate(Stage3);
+                break;
+        }
+
+        Stage.transform.position = StagePosition;
+
+    }
+}
