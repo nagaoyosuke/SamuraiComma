5e9a3ac5fbce7716a299e31cbf25af2acb876ada
shota okazaki <hw17a038@oecu.jp>
2019-11-11 17:38:45 +0900

    JoyConを振る動作を認識できるようにするソースコードおよびバンドル

---
+++ Assets/Scripts/Manager/CommandManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandManager : MonoBehaviour
+{
+    private List<Joycon> m_joycons;
+    private Joycon joyconL;
+    private Joycon joyconR;
+    public bool Lswing;
+    public bool Rswing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_joycons = JoyconManager.Instance.j;
+        joyconL = m_joycons.Find(c => c.isLeft);
+        joyconR = m_joycons.Find(c => !c.isLeft);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 accelL = joyconL.GetGyro();
+        Vector3 accelR = joyconR.GetGyro();
+        //コントローラーから方向を受け取る部分
+        //RswingおよびLswingで判定する
+        if (accelL.x >= 1.0|| accelL.y >= 1.0|| accelL.z >= 1.0)
+        {
+            Rswing = false;
+            Lswing = true;
+        }
+        if (accelR.x >= 1.0 || accelR.y >= 1.0 || accelR.z >= 1.0)
+        {
+            Lswing = false;
+            Rswing = true;
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Manager/SwingManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwingManager : MonoBehaviour
+{
+    public GameObject LRmoji = null;
+    [SerializeField] private CommandManager _commandManager;
+    private Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = LRmoji.GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //JoyCon(R)、JoyCon(L)を振ると右、左と出力
+        if (_commandManager.Rswing)
+        {
+            
+            text.text = "右";
+        }
+        if (_commandManager.Lswing)
+        {
+            
+            text.text = "左";
+        }
+    }
+}
