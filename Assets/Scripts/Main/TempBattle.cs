7754938d0d0016602ac5fa9c56018c9601aff207
o <hw17a114@oecu.jp>
2019-11-16 23:37:34 +0900

    joy-conを振っても居合処理を動作するように変更(仮)

--- Assets/Scripts/Main/TempBattle.cs
+++ Assets/Scripts/Main/TempBattle.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using SamuraiComma.Main.Manager;
+using UniRx;
+using Zenject;
 
 //joy-conのやつができるまでの繋ぎ
 public class TempBattle : MonoBehaviour
@@ -13,11 +16,30 @@ public class TempBattle : MonoBehaviour
     public ScreenFader screenFader;
     public TempData temp;
 
+    public CommandManager commandManager;
+    [Inject] private GameStateManager _gameStateManager;
+
+    private IntReactiveProperty _joycon = new IntReactiveProperty(0);
+    public IReadOnlyReactiveProperty<int> joycon => _joycon;
 
     // Start is called before the first frame update
     void Start()
     {
         Sound.PlayBgm("wind");
+
+        //joyconを振ると動作(仮)
+        _joycon
+            .DistinctUntilChanged()
+            .Where(_ => _gameStateManager.CurrentGameState.Value == GameState.Battle)
+            .Subscribe(_ => OnClickedTempButton());
+
+
+    }
+
+    private void Update()
+    {
+        _joycon.Value = commandManager.SwingLine;
+
     }
 
     public void OnClickedTempButton(){
