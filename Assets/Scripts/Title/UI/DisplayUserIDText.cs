8ba6610e7606fe03040e3c4c562e9ece6c4c1086
o <hw17a114@oecu.jp>
2020-04-18 01:31:29 +0900

    titleシーンで画面左上にアカウントのIDを表示する処理

---
+++ Assets/Scripts/Title/UI/DisplayUserIDText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SamuraiComma;
+using Zenject;
+
+namespace SamuraiComma.Title.UI
+{
+    /// <summary>
+    /// 画面左上にアカウントのIDを表示する。 toyoda 0418
+    /// </summary>
+    public class DisplayUserIDText : MonoBehaviour
+    {
+        [SerializeField] private Text _text;
+        [Inject] private SaveDataManager _saveDataManager;
+
+        private void Start()
+        {
+            _text.text = "Account ID:" + _saveDataManager.saveData.userID;
+        }
+    }
+}
+
+
